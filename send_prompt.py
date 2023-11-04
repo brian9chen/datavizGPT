6d2aae66ae410e01f0b9474fcb5e5063756cef3f
Chris Oswald <68239901+cdoswald@users.noreply.github.com>
2023-11-04 13:20:06 -0400

    minor formatting changes

--- send_prompt.py
+++ send_prompt.py
@@ -1,15 +1,15 @@
+"""Functions to send prompt and return output from OpenAI API."""
+
 import os
 import openai
 
 openai.api_key = os.environ["OPENAI_API_KEY"]
 
-# Define functions
 def get_completion(prompt, model="gpt-3.5-turbo"):
     messages = [{"role": "user", "content": prompt}]
     response = openai.ChatCompletion.create(
         model=model,
         messages=messages,
-        temperature=0, # this is the degree of randomness of the model's output
+        temperature=0, # degree of randomness of the model's output
     )
     return response.choices[0].message["content"]
-
