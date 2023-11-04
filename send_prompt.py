b4f562002617d0600627c44f2c34b4ec256bd4c9
Chris Oswald <68239901+cdoswald@users.noreply.github.com>
2023-11-04 12:40:26 -0400

    initial commit for send_prompt.py to forked version

---
+++ send_prompt.py
@@ -0,0 +1,15 @@
+import os
+import openai
+
+openai.api_key = os.environ["OPENAI_API_KEY"]
+
+# Define functions
+def get_completion(prompt, model="gpt-3.5-turbo"):
+    messages = [{"role": "user", "content": prompt}]
+    response = openai.ChatCompletion.create(
+        model=model,
+        messages=messages,
+        temperature=0, # this is the degree of randomness of the model's output
+    )
+    return response.choices[0].message["content"]
+
